56b043c27ff5212ef812d89806d08aa658ec4be3
Tomer27cz <tomer19cz@gmail.com>
2026-04-03 23:51:13 +0200

    fix

--- components/dlms_meter_lib/text_sensor/__init__.py
+++ components/dlms_meter_lib/text_sensor/__init__.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
 
-from . import DlmsMeterLibComponent, obis_code, CONF_DLMS_METER_ID, CONF_OBIS_CODE
+from .. import DlmsMeterLibComponent, obis_code, CONF_DLMS_METER_ID, CONF_OBIS_CODE
 
 DEPENDENCIES = ["dlms_meter_lib"]
 
